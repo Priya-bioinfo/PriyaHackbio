3021cee06a4d186bf4b45b5d3d4551dbda0be9a7
Priya-bioinfo <shanmugapriyab18@gmail.com>
2021-08-02 18:02:40 +0530

    adding the task file

---
+++ task1_Priya.py
@@ -0,0 +1,11 @@
+#usr/bin/python
+
+Name=['Shanmuga Priya B']
+Email=['shanmugapriyab18@gmail.com']
+Slackname= ['Priya']
+Biostack= ['Genomics','Data analysis']
+
+print("Name:",Name[0], sep="")
+print("Email:",Email[0],sep="")
+print("Slackname:@",Slackname[0],sep="")
+print("Biostack:",Biostack[0],sep="")
